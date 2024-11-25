79278eec2633d4f7e3c770d9b31377aba21223d8
SirEmitR <emit3reyes@gmail.com>
2024-11-24 20:14:29 -0600

    Add createFranchise function and route for franchise creation

--- controllers/v1/equipos.js
+++ controllers/v1/equipos.js
@@ -3,6 +3,22 @@ import ApiResponse from "../../models/api-response.js";
 import DatabaseResponse from "../../models/database-response.js";
 import normalize from "../../utils/normalize.js";
 
+async function createFranchise(req, res){
+    const { franquicia, logo, tipoFondo, fondo, creador } = req.body;
+    try{
+        if(!franquicia || !tipoFondo || !fondo || !creador){
+            throw new Error('Todos los campos son requeridos');
+        }
+        const normalized = normalize(franquicia);
+        const query = 'CALL CreateFranchise(?, ?, ?, ?, ?, ?);';
+        const ress = await database.query(query, [franquicia, normalized,creador,tipoFondo,fondo, logo]);
+        const data = new DatabaseResponse(ress, false, 'No se pudo crear la franquicia').data;
+        res.json(data);
+    }catch(err){
+        res.json(new ApiResponse().error(err.message));
+    }
+}
+
 async function validateFranchiseName(req, res){
     const { nombre } = req.body;
     try{
@@ -15,11 +31,11 @@ async function validateFranchiseName(req, res){
         const data = new DatabaseResponse(ress, false, 'El nombre de la franquicia ya existe').data;
         res.json(data);
     }catch(err){
-        console.log(err);
         res.json(new ApiResponse().error(err.message));
     }
 }
 
 export default {
+    createFranchise,
     validateFranchiseName
 };
--- routes/v1/equipos.js
+++ routes/v1/equipos.js
@@ -2,6 +2,7 @@ import express from 'express';
 const router = express.Router();
 import equipos from '../../controllers/v1/equipos.js';
 
+router.post('/', equipos.createFranchise);
 router.post('/verifyName', equipos.validateFranchiseName);
 
 export default router;
